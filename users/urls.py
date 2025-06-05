4a60aecc4d7e8b965353db38c39973191b90a1e0
LNoonan3 <lucynoonan3@gmail.com>
2025-06-06 00:00:12 +0100

    fix the user authentication template

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from .views import signup_view, profile_edit
+from django.urls import path, include
 from . import views
 
 app_name = 'users'
 
 urlpatterns = [
-    # path('signup/', signup_view, name='signup'),
+    path('accounts/', include('allauth.urls')),
     path('profile/', views.profile_edit, name='profile'),
     path('profile/edit/', profile_edit, name='profile_edit'),
 ]
