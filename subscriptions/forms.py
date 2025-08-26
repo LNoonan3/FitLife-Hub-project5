0d4c64fdcfb6080915586b9f571a30f93c7344b2
LNoonan3 <lucynoonan3@gmail.com>
2025-08-26 20:04:42 +0100

    deleted subscription payment checkout page and revert back to the sandbox payment

--- subscriptions/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-
-class SubscriptionCheckoutForm(forms.Form):
-    full_name = forms.CharField(max_length=100, label="Full Name")
-    email = forms.EmailField(label="Email")
